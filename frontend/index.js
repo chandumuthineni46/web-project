5b836c7b428a54179237764f10013b3f01b7ced5
chandumuthineni46 <chandumuthineni46@gmail.com>
2026-08-13 18:48:23 +0530

    added some additional feat

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,29 @@
-alert("hello fron javascript")
+const users = [
+ {
+   "name" : "Jonn Doe",
+   "gender" : "Male",
+   "image" : "john(1).png"
+ },
+   {
+   "name" : "Jane Doe",
+   "gender" : "Female",
+   "image" : "jane.png"
+ }
+];
+
+
+var curIndex = 0;
+
+
+function toggle(){
+if(curIndex == 0)
+   curIndex = 1;
+else
+   curIndex = 0;
+
+
+document.getElementById("card-image").src = users[curIndex].image;
+document.getElementById("card-name").innerText =users[curIndex].name;
+document.getElementById("card-gender").innerText =users[curIndex].gender;
+ //gender needs to be updated
+}
